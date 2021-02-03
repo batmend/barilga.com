59cbbcdd8cb02642ffc5f5c924ca1ded761e60f2
Batmend Ganbat <batmend.g@gmail.com>
2021-02-04 02:58:11 +0800

    top tur haav

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Test from "../components/layout/Test";
 import GoToTop from "../components/GoToTop";
 // import Header from "../components/layout/Header";
@@ -12,6 +13,12 @@ export default function Home() {
         <title>Барилгын материал худалдаа</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1>
+        Read{" "}
+        <Link href="/posts/first-post">
+          <a>this page!</a>
+        </Link>
+      </h1>
       <Test />
       <GoToTop />
       {/* <Header /> */}
